9c4ce9706df8f82e9eebc24577b4e2d41cd909da
Jakub Hlusička <limeth@protonmail.com>
2017-07-15 18:18:19 +0200

    Implement the Error type and getter macros

---
+++ src/error.rs
@@ -0,0 +1,79 @@
+use std;
+use std::fmt;
+use ffi;
+
+#[derive(Debug)]
+pub enum ErrorKind {
+    General,
+    InvalidFormat,
+    InvalidParameter,
+    NotSupported,
+    DeviceUnavailable,
+}
+
+impl ErrorKind {
+    pub fn from_code(other: i32) -> Option<Self> {
+        match other {
+            x if x == ffi::CUBEB_OK as i32 => None,
+            x if x == ffi::CUBEB_ERROR as i32 => Some(ErrorKind::General),
+            x if x == ffi::CUBEB_ERROR_INVALID_FORMAT as i32 => Some(ErrorKind::InvalidFormat),
+            x if x == ffi::CUBEB_ERROR_INVALID_PARAMETER as i32 => Some(ErrorKind::InvalidParameter),
+            x if x == ffi::CUBEB_ERROR_NOT_SUPPORTED as i32 => Some(ErrorKind::NotSupported),
+            x if x == ffi::CUBEB_ERROR_DEVICE_UNAVAILABLE as i32 => Some(ErrorKind::DeviceUnavailable),
+            code @ _ => panic!("An unexpected error Cubec occured: {}", code),
+        }
+    }
+}
+
+impl ErrorKind {
+    pub fn description(&self) -> &'static str {
+        match *self {
+            ErrorKind::General => "General Cubeb error; something went wrong",
+            ErrorKind::InvalidFormat => "Invalid format",
+            ErrorKind::InvalidParameter => "Invalid parameter",
+            ErrorKind::NotSupported => "Operation not supported",
+            ErrorKind::DeviceUnavailable => "Device unavailable",
+        }
+    }
+}
+
+impl fmt::Display for ErrorKind {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.description())
+    }
+}
+
+#[derive(Debug)]
+pub enum Error {
+    Cubeb(ErrorKind)
+}
+
+impl Error {
+    pub fn cubeb(code: i32) -> Option<Self> {
+        ErrorKind::from_code(code).map(Error::Cubeb)
+    }
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            Error::Cubeb(ref kind) => write!(f, "Cubeb error: {}", kind),
+        }
+    }
+}
+
+impl std::error::Error for Error {
+    fn description(&self) -> &str {
+        // Both underlying errors already impl `Error`, so we defer to their
+        // implementations.
+        match *self {
+            Error::Cubeb(ref kind) => kind.description(),
+        }
+    }
+
+    fn cause(&self) -> Option<&std::error::Error> {
+        match *self {
+            Error::Cubeb(_) => None,
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -2,22 +2,88 @@
 #![allow(non_camel_case_types)]
 #![allow(non_upper_case_globals)]
 #![allow(non_snake_case)]
+#![feature(concat_idents)]
 
 extern crate libc;
 
 mod ffi;
+pub mod error;
+
+use std::mem;
+use std::ffi::CStr;
+use std::ffi::CString;
+use error::Error;
+
+pub struct Cubeb {
+    context: *mut ffi::cubeb,
+}
+
+macro_rules! getter_body {
+    ($ffi_prefix:ident, $field:expr, $name:ident) => {
+        unsafe {
+            use ffi::*; // Maybe proc macros can be used to avoid this
+            let result = &mut mem::uninitialized() as *mut _;
+            let error_code = concat_idents!($ffi_prefix, $name)($field, result);
+
+            if let Some(error) = Error::cubeb(error_code) {
+                Err(error)
+            } else {
+                Ok(*result)
+            }
+        }
+    }
+}
+macro_rules! getter {
+    ($ffi_prefix:ident, $field:ident, $name:ident, $ret:ty) => {
+        pub fn $name(&self) -> Result<$ret, Error> {
+            getter_body!($ffi_prefix, self.$field, $name)
+        }
+    }
+}
+
+impl Cubeb {
+    pub fn new<T: Into<Vec<u8>>>(name: T) -> Self {
+        let mut context: *mut ffi::cubeb = ::std::ptr::null_mut();
+        let context_name = CString::new(name).unwrap().as_ptr();
+
+        unsafe {
+            ffi::cubeb_init(&mut context as *mut *mut ffi::cubeb, context_name, ::std::ptr::null());
+        }
+
+        Cubeb {
+            context,
+        }
+    }
+
+    pub fn get_backend_id(&self) -> &str {
+        unsafe {
+            CStr::from_ptr(ffi::cubeb_get_backend_id(self.context))
+                .to_str()
+                .expect("The backend ID isn't a UTF-8 string for some reason.")
+        }
+    }
+
+    getter!(cubeb_, context, get_max_channel_count, u32);
+    getter!(cubeb_, context, get_preferred_sample_rate, u32);
+}
+
+impl Drop for Cubeb {
+    fn drop(&mut self) {
+        unsafe {
+            ffi::cubeb_destroy(self.context);
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
-    use super::ffi;
-    use ::std::ffi::CString;
+    use super::*;
 
     #[test]
     fn it_works() {
-        unsafe {
-            let mut context: *mut ffi::cubeb = ::std::ptr::null_mut();
-            let context_name = CString::new("cubeb-rs-test").unwrap().as_ptr();
-            ffi::cubeb_init(&mut context as *mut *mut ffi::cubeb, context_name, ::std::ptr::null());
-        }
+        let ctx = Cubeb::new("cubeb-rs-test");
+        println!("get_backend_id: {}", ctx.get_backend_id());
+        println!("get_max_channel_count: {:?}", ctx.get_max_channel_count());
+        println!("get_preferred_sample_rate: {:?}", ctx.get_preferred_sample_rate());
     }
 }
